aad088c2258a05f8fb87f04b6bb00a5d0352f6bf
Rodrigo Santos <Rodrigo.Santos@tradenation.com>
2021-06-07 16:08:35 +0100

    boilerplate init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ title = 'Texto' }: any) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">{title}</a>
         </h1>
 
         <p className={styles.description}>
