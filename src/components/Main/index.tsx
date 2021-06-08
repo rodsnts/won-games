ed73261fad1c1d1615c6b8eab2eec284f66e61d6
Rodrigo Santos <Rodrigo.Santos@tradenation.com>
2021-06-08 15:41:06 +0100

    Add Hero section

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,20 @@
-const Main = () => {
-  return (
-    <main>
-      <h1>Advanced React</h1>
-    </main>
-  )
-}
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Atom image and Advanced React writen aside"
+    />
+    <S.Title>Advanced React</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS and Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="A developer facing his computer"
+    />
+  </S.Wrapper>
+)
 
 export default Main
